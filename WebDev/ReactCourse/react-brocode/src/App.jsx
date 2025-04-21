1ed1b67208ec6fe2e6f40fa7225f16a42729bfca
SohamAmbore <soham.ambore1234@gmail.com>
2025-04-21 19:40:46 +0530

    React course

---
+++ WebDev/ReactCourse/react-brocode/src/App.jsx
@@ -0,0 +1,13 @@
+import Button from './Button/Button.jsx';
+import Card from './Card.jsx'
+
+function App() {
+  return (
+    <>
+    <Card />
+    <Button />
+    </>
+  );
+}
+
+export default App
