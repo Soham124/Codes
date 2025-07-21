edd621fdde8975315a852f65395e68c495f3aff5
Soham <soham.ambore1234@gmail.com>
2025-07-21 12:08:05 +0530

    added python course files

---
+++ UdemyPython/test.py
@@ -0,0 +1,17 @@
+# print(2+3)
+
+
+# age = 24
+# print(type(age))
+
+# name = "soham"
+# print(type(name))
+
+# age = name
+# print(type(age))
+
+
+num1 = int(input())
+num2 = int(input("Enter the second number: "))
+
+print(num1 + num2)
