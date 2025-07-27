c3a7ba4ee7e15365043fed079be3b8876ae237c3
Soham <soham.ambore1234@gmail.com>
2025-07-27 10:10:44 +0530

    new copy file and solved assignment questions

--- UdemyPython/test.py
+++ UdemyPython/test.py
@@ -11,7 +11,24 @@
 # print(type(age))
 
 
-num1 = int(input())
-num2 = int(input("Enter the second number: "))
+# num1 = int(input())
+# num2 = int(input("Enter the second number: "))
 
-print(num1 + num2)
+# print(num1 + num2)
+
+
+# for i in range (1, 6):
+#     for j in range (1, 6):
+#         print("*", end="")
+#     print()
+
+num = int(input())
+first = 0
+second = 1
+
+for i in range (0, num+1):
+    print (first)
+
+    temp = second
+    second = first + second
+    first = temp 
