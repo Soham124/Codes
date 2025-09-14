9ea45b172c201e7be73ca8ed031259a7988f89c6
Soham <soham.ambore1234@gmail.com>
2025-09-14 15:27:34 +0530

    hashing java code

--- DSA/hashing.java
+++ DSA/hashing.java
@@ -1,7 +1,18 @@
 package DSA;
+import java.util.*;
 
 public class hashing {
     public static void main(String[] args) {
-        
+        HashMap<String, Integer> EmpID = new HashMap<>();
+        EmpID.put("Soham", 69);
+        EmpID.put("John", 46);
+        System.out.println(EmpID);
+
+        int[] myArray = {1, 2, 3, 4, 5};
+        boolean toggle = false;
+
+        for (int i = 0; i < myArray.length; i++) {
+            System.out.println(myArray[i]);
+        }
     }
 }
