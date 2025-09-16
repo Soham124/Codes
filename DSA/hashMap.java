14be142b1a9d538a8d13151e0cc5e54f85a9aca6
Soham <soham.ambore1234@gmail.com>
2025-09-17 04:17:57 +0530

    javaLeetcode

--- DSA/hashMap.java
+++ DSA/hashMap.java
@@ -8,11 +8,5 @@ public class hashMap {
         EmpID.put("John", 46);
         System.out.println(EmpID);
 
-        int[] myArray = {1, 2, 3, 4, 5};
-        boolean toggle = false;
-
-        for (int i = 0; i < myArray.length; i++) {
-            System.out.println(myArray[i]);
-        }
     }
 }
---
+++ DSA/temporary.java
@@ -0,0 +1,27 @@
+package DSA;
+import java.util.*;
+
+public class temporary {
+    public static void main(String[] args) {
+        int [] nums = {11,7,15,2};
+        int target = 9;
+
+        int num1 = 0;
+        int num2 = 0;
+        boolean toggle = false;
+
+        for (int i = 0; i < nums.length; i++) {
+            num1 = nums[i];
+            System.out.println("This is num1: "+ num1);
+            for (int j = 0; j < nums.length; j++) {
+                if(num1+nums[j] == target){
+                    num2 = nums[j];
+                    System.out.println("This is num2: "+ num2);
+                }
+            }
+            if(toggle) break;
+        }
+        System.out.println(num1+" "+ num2);
+    
+    }
+}
