22a93c7493b3bf70796ac63ceeb95902c9ffedac
Soham <soham.ambore1234@gmail.com>
2025-09-21 11:29:46 +0530

    hashmap

--- DSA/hashMap.java
+++ DSA/hashMap.java
@@ -3,10 +3,18 @@ import java.util.*;
 
 public class hashMap {
     public static void main(String[] args) {
-        HashMap<String, Integer> EmpID = new HashMap<>();
-        EmpID.put("Soham", 69);
-        EmpID.put("John", 46);
-        System.out.println(EmpID);
+        HashMap<Integer, String> map = new HashMap<>();
+        map.put(1, "Red");
+        map.put(2, "Green");
+        map.put(3, "Black");
+        map.put(4, "White");
+        map.put(5, "Blue");
+        System.out.println(map);
+        System.out.println(map.size()); //size of the hashmap(no of pairs)
+        if(map.containsKey(4)){
+            System.out.println(map.get("Green")); //prints value of key Green if it exists
+        }
+
 
     }
 }
